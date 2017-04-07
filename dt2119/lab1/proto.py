8809dfa61c87c8b913b9db6e4ed2aec6f3168cd6
Martin Hwasser <martin.hwasser@gmail.com>
2017-04-07 09:48:59 +0200

    finished lab1 of dt2219

--- dt2119/lab1/proto.py
+++ dt2119/lab1/proto.py
@@ -1,6 +1,12 @@
 # DT2118, Lab 1 Feature Extraction
 
 # Function given by the exercise ----------------------------------
+import numpy as np
+from scipy.fftpack import fft
+from scipy.fftpack.realtransforms import dct
+from scipy.signal import lfilter,hamming
+from tools import lifter,trfbank
+from tools import trfbank
 
 def mfcc(samples, winlen = 400, winshift = 200, preempcoeff=0.97, nfft=512, nceps=13, samplingrate=20000, liftercoeff=22):
     """Computes Mel Frequency Cepstrum Coefficients.
@@ -39,6 +45,13 @@ def enframe(samples, winlen, winshift):
         numpy array [N x winlen], where N is the number of windows that fit
         in the input signal
     """
+    N = int(np.ceil(float(np.abs(len(samples) - winlen)) / winshift))
+    y = np.empty((N, winlen))
+
+    for i in xrange(y.shape[0]):
+        n = i * winshift
+        y[i,:] = samples[n:n+winlen]
+    return y
     
 def preemp(input, p=0.97):
     """
@@ -67,9 +80,8 @@ def windowing(input):
     Note (you can use the function hamming from scipy.signal, include the sym=0 option
     if you want to get the same results as in the example)
     """
-
-    hamming_window = hamming(preemph.shape[1], sym=False)
-    windowed = input *  hamming_window
+    hamming_window = hamming(input.shape[1], sym=False)
+    windowed = input * hamming_window
     return windowed
 
 def powerSpectrum(input, nfft):
@@ -84,7 +96,7 @@ def powerSpectrum(input, nfft):
         array of power spectra [N x nfft]
     Note: you can use the function fft from scipy.fftpack
     """
-    spec = np.absolute(fftpack.fft(example['windowed'], NFFT))
+    spec = np.absolute(fft(input, nfft))
     return np.square(spec)
 
 def logMelSpectrum(input, samplingrate):
@@ -101,6 +113,8 @@ def logMelSpectrum(input, samplingrate):
     Note: use the trfbank function provided in tools.py to calculate the filterbank shapes and
           nmelfilters
     """
+    filter_bank = trfbank(samplingrate, input.shape[1])
+    return np.log(input.dot(filter_bank.T))
 
 def cepstrum(input, nceps):
     """
@@ -114,6 +128,8 @@ def cepstrum(input, nceps):
         array of Cepstral coefficients [N x nceps]
     Note: you can use the function dct from scipy.fftpack.realtransforms
     """
+    mfcc = dct(input, norm='ortho')[:,:nceps]
+    return mfcc
 
 def dtw(x, y, dist):
     """Dynamic Time Warping.
@@ -131,3 +147,86 @@ def dtw(x, y, dist):
 
     Note that you only need to define the first output for this exercise.
     """
+    r, c = len(x), len(y)
+    D0 = np.zeros((r + 1, c + 1))
+    D0[0, 1:] = np.inf
+    D0[1:, 0] = np.inf
+    acc_dist = D0[1:, 1:] # view
+    acc_dist = dist(x, y)
+    #for i in range(r):
+    #    for j in range(c):
+    #        print x[i].shape
+    #        print y[j].shape
+    #        distance = dist(x[i], y[j])
+    #        print distance
+    #        acc_dist[i, j] = distance
+    local_dist = acc_dist.copy()
+    for i in range(r):
+        for j in range(c):
+            acc_dist[i, j] += min(D0[i, j], D0[i, j+1], D0[i+1, j])
+    if len(x) == 1:
+        path = np.zeros(len(y)), range(len(y))
+    elif len(y) == 1:
+        path = range(len(x)), np.zeros(len(x))
+    else:
+        path = _traceback(D0)
+    return acc_dist[-1, -1] / sum(acc_dist.shape), local_dist, acc_dist, path
+
+def _traceback(D):
+    i, j = array(D.shape) - 2
+    p, q = [i], [j]
+    while ((i > 0) or (j > 0)):
+        tb = argmin((D[i, j], D[i, j+1], D[i+1, j]))
+        if (tb == 0):
+            i -= 1
+            j -= 1
+        elif (tb == 1):
+            i -= 1
+        else: # (tb == 2):
+            j -= 1
+        p.insert(0, i)
+        q.insert(0, j)
+    return array(p), array(q)
+
+def mspec(samples, winlen=400, winshift=200, preempcoeff=0.97, nfft=512, nceps=13, samplingrate=20000, liftercoeff=22,
+          enable_plot=False):
+    """Computes Mel Frequency Cepstrum Coefficients.
+    Args:
+        samples: array of speech samples with shape (N,)
+        winlen: lenght of the analysis window
+        winshift: number of samples to shift the analysis window at every time step
+        preempcoeff: pre-emphasis coefficient
+        nfft: length of the Fast Fourier Transform (power of 2, >= winlen)
+        nceps: number of cepstrum coefficients to compute
+        samplingrate: sampling rate of the original signal
+        liftercoeff: liftering coefficient used to equalise scale of MFCCs
+    Returns:
+        N x nceps array with lifetered MFCC coefficients
+    """
+    frames = enframe(samples, winlen, winshift)
+    preemph = preemp(frames, preempcoeff)
+    windowed = windowing(preemph)
+    spec = powerSpectrum(windowed, nfft)
+    mspec = logMelSpectrum(spec, samplingrate)
+    if (enable_plot):
+        fig = plt.figure(figsize=(12, 6))
+        ax1 = fig.add_subplot(421)
+        ax1.plot(samples)
+        ax2 = fig.add_subplot(422)
+        ax2.pcolormesh(frames)
+        ax3 = fig.add_subplot(423)
+        ax3.pcolormesh(preemph)
+        ax4 = fig.add_subplot(424)
+        ax4.pcolormesh(windowed)
+        ax5 = fig.add_subplot(425)
+        ax5.pcolormesh(spec)
+        ax6 = fig.add_subplot(426)
+        ax6.pcolormesh(mspec)
+        # ax7 = fig.add_subplot(427)
+        # ax7.pcolormesh(ceps)
+        #
+        # ax8 = fig.add_subplot(428)
+        # ax8.pcolormesh(lifted)
+        plt.tight_layout()
+        plt.show()
+    return mspec
