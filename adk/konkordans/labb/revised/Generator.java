40c8329d8ede0d796dcb4b3c1d0dda7370c95697
Martin Hwasser <martin.hwasser@gmail.com>
2017-10-08 20:22:22 +0200

    Rename from course codes to course names.

--- adk/konkordans/labb/revised/Generator.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class Generator {
-	
-	public static void main(String[] args) throws IOException{
-		IndexBinary.generateBinaryIndex();
-		GeneratePrefixFile.generatePrefixFile();
-	}
-}
